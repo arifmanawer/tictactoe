7137d8c660aac259389bb6b7cc97d9cd39d1ed4e
arifmanawer <112596030+arifmanawer@users.noreply.github.com>
2023-01-14 12:35:56 -0500

    board

--- tictactoe.py
+++ tictactoe.py
@@ -6,3 +6,13 @@ choices = ["X", "O"]
 if player1Mark not in choices:
     while player1Mark not in choices:
         player1Mark = input("Please enter valid choice. (X or O) ")
+if player1Mark == "X":
+    player2mark = "O"
+else:
+    player2mark = "X"
+
+board = []
+for row in range(3):
+    board.append([""] * 3)
+for row in range(3):
+    print(board[row])
