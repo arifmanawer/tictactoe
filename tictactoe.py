3e1adc707a1917e9a883bee9f48e93ccbb7013ab
arifmanawer <112596030+arifmanawer@users.noreply.github.com>
2023-01-14 11:50:56 -0500

    user input

--- tictactoe.py
+++ tictactoe.py
@@ -1,1 +1,8 @@
-print("will actually start now")
+# Game Step
+# get user input
+
+player1Mark = input("Please choose X or O: ")
+choices = ["X", "O"]
+if player1Mark not in choices:
+    while player1Mark not in choices:
+        player1Mark = input("Please enter valid choice. (X or O) ")
